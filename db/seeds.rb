d28089835e8abd47f6b39c2efb5c5976c95f58d8
nanasuzi <kamizyou3@gmail.com>
2021-07-07 12:29:05 +0000

    [UPDATE]都道府県追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(email: 'kamizyou3@gmail.com',
-              password: 'yuta8223'
-              )
+ #Admin.create!(email: 'kamizyou3@gmail.com',
+  #            password: 'yuta8223'
+   #           )
+
+Theme.create(name: "北海道")
+Theme.create(name: "青森")
+Theme.create(name: "岩手")
+Theme.create(name: "宮城")
+Theme.create(name: "秋田")
+Theme.create(name: "山形")
+Theme.create(name: "福島")
+Theme.create(name: "茨城")
+Theme.create(name: "栃木")
+Theme.create(name: "群馬")
+Theme.create(name: "埼玉")
+Theme.create(name: "千葉")
+Theme.create(name: "東京")
+Theme.create(name: "神奈川")
+Theme.create(name: "新潟")
+Theme.create(name: "富山")
+Theme.create(name: "石川")
+Theme.create(name: "福井")
+Theme.create(name: "山梨")
+Theme.create(name: "長野")
+Theme.create(name: "岐阜")
+Theme.create(name: "静岡")
+Theme.create(name: "愛知")
+Theme.create(name: "三重")
+Theme.create(name: "滋賀")
+Theme.create(name: "京都")
+Theme.create(name: "大阪")
+Theme.create(name: "兵庫")
+Theme.create(name: "奈良")
+Theme.create(name: "和歌山")
+Theme.create(name: "鳥取")
+Theme.create(name: "島根")
+Theme.create(name: "岡山")
+Theme.create(name: "広島")
+Theme.create(name: "山口")
+Theme.create(name: "徳島")
+Theme.create(name: "香川")
+Theme.create(name: "愛媛")
+Theme.create(name: "高知")
+Theme.create(name: "福岡")
+Theme.create(name: "佐賀")
+Theme.create(name: "長崎")
+Theme.create(name: "熊本")
+Theme.create(name: "大分")
+Theme.create(name: "宮崎")
+Theme.create(name: "鹿児島")
+Theme.create(name: "沖縄")
+
+
+
+
+
