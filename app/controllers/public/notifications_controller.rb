b95f2c2d9e565766a87cd688b30ea1fee8fa99e6
nanasuzi <kamizyou3@gmail.com>
2021-07-22 13:39:30 +0000

    [FIX]通知データ取得コードをコントローラーへ記述変更

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,6 +1,7 @@
 class Public::NotificationsController < ApplicationController
   def index
     @notifications = current_user.passive_notifications.all
+    @notifications_check = @notifications.where.not(visitor_id: current_user.id)
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
